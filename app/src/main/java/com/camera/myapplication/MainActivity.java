fe494b1e768db8f306094ba9a722d7b73935d54b
umoriha <kxhoumu9x1231@hotmail.co.jp>
2021-08-22 21:34:46 +0900

    add code

--- app/src/main/java/com/camera/myapplication/MainActivity.java
+++ app/src/main/java/com/camera/myapplication/MainActivity.java
@@ -1,21 +1,42 @@
 package com.camera.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
-import android.content.MediaStore;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     private final static int REQUEST_CAMERA = 20210822;
+    public String currentPhotoPath;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (intent.resolveActivity(detPackageManager()) != null) {
-            startActivityForResult(intent, REQUEST_CAMERA);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+
+            File PhotoFile = null;
+            try {
+                PhotoFile = createImageFile();
+            } catch (IOException ex) {
+                System.out.println("未実装");
+            }
+            if (PhotoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(MainActivity.this,
+                        getApplicationContext().getPackageName() + ".fileprovider",PhotoFile);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(intent, REQUEST_CAMERA);
+            }
         }
     }
     @Override
@@ -23,8 +44,21 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_CAMERA) {
             if(data.getExtras() != null) {
-
+                //保存処理
+                System.out.print("未実装");
             }
         }
+    }
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+        currentPhotoPath = image.getAbsolutePath();
+        return  image;
     }
 }
